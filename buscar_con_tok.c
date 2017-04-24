3ce7f65a130d10353c7a119b5eb286dd576803f7
andreaestefania12 <andreaestefania12@javerianacali.edu.co>
2017-04-24 18:03:33 -0500

    diccionario

---
+++ buscar_con_tok.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+//#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main(){//Buscar archivo
+    FILE * Diccionario = fopen("Diccionario.txt","r");
+    char guardar[1000];
+    char palabra[100];//lo que deseo buscar
+
+    printf("Digite la palabra que desea buscar: ");
+    scanf("%s",palabra);//tomo lo que me mandan  escriben.
+    char *y=palabra;
+    if(Diccionario==NULL){
+        printf("No se pudo leer: :( .....");
+        exit(0);
+    }
+    else{
+        while(!(feof(Diccionario))){
+            fgets(guardar,1000,Diccionario);//donde se guarda,espacio,de donde lo lee.
+            char *x;
+            char *y;
+            x=strstr(guardar,palabra);
+            if(strstr(guardar,palabra)!=NULL){//strstr
+                strtok(x," : ");
+                strtok(NULL," ");
+                y=strtok(NULL,"\n");
+
+                printf("%s",x);
+                printf("\n");
+                //printf("%s",y);
+
+
+
+
+
+            }
+
+        }
+    }
+    fclose(Diccionario);
+    //getch();
+
+}
